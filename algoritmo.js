8031ddf290fee6cbcc4af4d7483f7a418fdf2334
Araceli Cabana <aracelicabana94@gmail.com>
2023-04-26 20:43:53 -0300

    practica1-5

---
+++ algoritmo.js
@@ -0,0 +1,18 @@
+document.querySelector("body").innerHTML = `
+<h1>Algoritmo como preparar un huevo frito</h1>
+    <img src="Imagen/OIP.jpg" alt="huevo frito" srcset="">
+
+    <ol>
+        <li>Abrir la heladera</li>
+        <li>Tomar un huevo crudo</li>
+        <li>Agarrar el sarten </li>
+        <li>Colocar aceite en el sarten</li>
+        <li>Encender el fuego de la cocina</li>
+        <li>Poner el sarten sobre el fuego</li>
+        <li>Esperar un minuto que caliente el aciete</li>
+        <li>Romper la cascara del huevo</li>
+        <li>Poner la yema y la clara en el sarten</li>
+        <li>Esperar unos minutos que el huevo se cocine</li>
+        <li>Sacar el huevo del sarten y servir en el plato.</li>
+    </ol>
+`
